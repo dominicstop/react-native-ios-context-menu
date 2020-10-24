75817b26fc114a22d02c3b6feed0e78ae47b77e0
Dominic Go <dominicstop@gmail.com>
2020-10-24 18:04:36 +0800

    ⭐️ Example - Added: `ContextMenuViewTest03`

--- example/src/App.js
+++ example/src/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 
 import { ContextMenuViewTest01 } from './components/ContextMenuViewTest01';
 import { ContextMenuViewTest02 } from './components/ContextMenuViewTest02';
+import { ContextMenuViewTest03 } from './components/ContextMenuViewTest03';
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
       <ScrollView contentContainerStyle={styles.scrollviewContainer}>
         <ContextMenuViewTest01/>
         <ContextMenuViewTest02/>
+        <ContextMenuViewTest03/>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ example/src/components/ContextMenuViewTest03.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { ExampleContextMenuItem } from './ExampleContextMenuItem';
+
+
+export class ContextMenuViewTest03 extends React.PureComponent {
+  render(){
+    return(
+      <ExampleContextMenuItem
+        index={3}
+        title={'Simple Example #3'}
+        desc={"Context menu with 1 action and 1 submenu (with 3 submenu actions)"}
+        onPressMenuItem={({key}) => alert(key)}
+        menuConfig={{
+          menuTitle: 'ContextMenuViewTest03',
+          menuItems: [{
+            actionKey  : 'key-01',
+            actionTitle: 'Action #1',
+            imageType  : 'SYSTEM',
+            imageValue : 'folder',
+          }, {
+            menuTitle: 'Submenu...',
+            menuItems: [{
+              actionKey  : 'key-01-01',
+              actionTitle: 'Submenu Action #1',
+              imageType  : 'SYSTEM',
+              imageValue : 'star',
+            }, {
+              actionKey  : 'key-01-02',
+              actionTitle: 'Submenu Action #2',
+              imageType  : 'SYSTEM',
+              imageValue : 'star.lefthalf.fill',
+            }, {
+              actionKey  : 'key-01-03',
+              actionTitle: 'Submenu Action #3',
+              imageType  : 'SYSTEM',
+              imageValue : 'star.fill',
+            }]
+          }],
+        }}
+      />
+    );
+  };
+};
+
+const styles = StyleSheet.create({
+});
