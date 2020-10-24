88133fb565226aa31d8d272502b4ae19e96145e8
Dominic Go <dominicstop@gmail.com>
2020-10-24 15:04:08 +0800

    ✨ Example - Minor Changes: `ContextMenuViewTest01 `

--- example/src/components/ContextMenuViewTest01.js
+++ example/src/components/ContextMenuViewTest01.js
@@ -4,21 +4,6 @@ import { StyleSheet } from 'react-native';
 import { ExampleContextMenuItem } from './ExampleContextMenuItem';
 
 
-const MenuConfig01 = {
-  menuTitle: 'Menu Test 1',
-  // can be another menu or an action
-  menuItems: [{
-    actionKey: 'key-01',
-    actionTitle: 'Action #1'
-  }, {
-    actionKey  : 'key-02'   ,
-    actionTitle: 'Action #2',
-  }, {
-    actionKey  : 'key-03'   ,
-    actionTitle: 'Action #3',
-  }],
-};
-
 export class ContextMenuViewTest01 extends React.PureComponent {
   render(){
     return(
@@ -26,7 +11,20 @@ export class ContextMenuViewTest01 extends React.PureComponent {
         index={1}
         title={'Simple Example #1'}
         desc={'Context menu with 3 actions (no icon, just text)'}
-        menuConfig={MenuConfig01}
+        onPressMenuItem={({key}) => alert(key)}
+        menuConfig={{
+          menuTitle: 'ContextMenuViewTest01',
+          menuItems: [{
+            actionKey  : 'key-01',
+            actionTitle: 'Action #1',
+          }, {
+            actionKey  : 'key-02'   ,
+            actionTitle: 'Action #2',
+          }, {
+            actionKey  : 'key-03'   ,
+            actionTitle: 'Action #3',
+          }],
+        }}
       />
     );
   };
