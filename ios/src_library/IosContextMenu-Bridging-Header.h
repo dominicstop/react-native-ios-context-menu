4cac7877499ab4ac1e1af6e86de500d5927b4dff
Dominic Go <dominicstop@gmail.com>
2020-11-19 04:15:34 +0800

    🛁 Cleaned up: `Bridging-Header`

--- ios/src_library/IosContextMenu-Bridging-Header.h
+++ ios/src_library/IosContextMenu-Bridging-Header.h
@@ -3,23 +3,9 @@
 #import <React/RCTDevLoadingView.h>
 #endif
 
-#import "RCTSwiftLog.h"
-
 #import <React/RCTBridge.h>
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventDispatcher.h>
-#import <React/RCTRootView.h>
-#import <React/RCTUtils.h>
-#import <React/RCTConvert.h>
-#import <React/RCTBundleURLProvider.h>
-
-#import <React/RCTViewManager.h>
 #import <React/RCTView.h>
 #import <React/RCTUIManager.h>
-#import <React/RCTTouchHandler.h>
-#import <React/RCTAssert.h>
-#import <React/UIView+React.h>
-#import <React/RCTTextView.h>
-#import <React/RCTTextShadowView.h>
-#import <React/RCTComponent.h>
+
+#import "RCTSwiftLog.h"
 #import <React/RCTLog.h>
