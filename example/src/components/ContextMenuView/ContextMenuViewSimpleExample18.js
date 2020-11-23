35bcf0e6d5557215c23b09274f14d2eed562faad
Dominic Go <dominicstop@gmail.com>
2020-11-23 09:35:13 +0800

    ✨ Example - Minor Changes

--- example/src/components/ContextMenuView/ContextMenuViewSimpleExample18.js
+++ example/src/components/ContextMenuView/ContextMenuViewSimpleExample18.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image, Text } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import { ExampleContextMenuItem } from '../ExampleContextMenuItem';
 
