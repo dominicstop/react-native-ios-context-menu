25bf0b2dc1159f730534553ddf61a4d84024a562
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2021-11-15 06:34:31 +0800

    ⭐️ Types - Partially Add Types For `ActionSheetFallback`

--- src/functions/ActionSheetFallback.ts
+++ src/functions/ActionSheetFallback.ts
@@ -1,52 +1,68 @@
-import { MenuActionKeys, MenuConfigKeys, MenuOptions, MenuElementAtrributes } from '../constants/Enums';
 import * as Helpers from './helpers';
 
+import type { MenuActionConfig, MenuConfig } from '../types/MenuConfig';
+
 // TODO: Refactor/cleanup...
+// TODO: Fix types
 
 class ActionSheetFallbackHelpers {
   /** is the object a `MenuAction` item */
-  static isObjectMenuAction(object){
+  static isObjectMenuAction(object: object): object is MenuActionConfig {
     return (
-      (object?.[MenuActionKeys.actionKey     ] != null) ||
-      (object?.[MenuActionKeys.actionTitle   ] != null) ||
-      (object?.[MenuActionKeys.menuAttributes] != null) ||
-      (object?.[MenuActionKeys.menuAttributes] != null)
+      // required properties --------
+      ('actionKey'      in object) ||
+      ('actionTitle'    in object) ||
+
+      // optional properties --------------
+      ('menuState'            in object) ||
+      ('menuAttributes'       in object) ||
+      ('discoverabilityTitle' in object) ||
+      ('icon'                 in object) 
     );
   };
   
   /** is the object a `MenuObject` item */
-  static isObjectMenuConfig(object){
+  static isObjectMenuConfig(object: object): object is MenuConfig {
     return (
-      (object?.[MenuConfigKeys.menuTitle  ] != null) ||
-      (object?.[MenuConfigKeys.menuItems  ] != null) ||
-      (object?.[MenuConfigKeys.menuOptions] != null)
+      // required properties --------
+      ('menuTitle'      in object) ||
+
+      // optional properties -----
+      ('menuOptions' in object) ||
+      ('menuItems'   in object) ||
+      ('icon'        in object) 
     );
   };
 
   /** based on the `indexPath`, return the corresponding `MenuConfig` item */
-  static getItemFromMenuConfig(menuConfig, indexPath){
+  static getItemFromMenuConfig(menuConfig: MenuConfig, indexPath: Array<number>){
     if(indexPath  == null) return null;
     if(menuConfig == null) return null;
 
-    let currentObject = menuConfig;
+    let currentObject: 
+      MenuConfig | MenuActionConfig | undefined = menuConfig;
 
     for (const index of indexPath) {
-      if(ActionSheetFallbackHelpers.isObjectMenuAction(currentObject)){
+      if(currentObject == null){
+        return null;
+
+      } else if(ActionSheetFallbackHelpers.isObjectMenuAction(currentObject)){
         return currentObject;
 
-      } else if (currentObject == null || !ActionSheetFallbackHelpers.isObjectMenuConfig(currentObject)){
+      } else if (!ActionSheetFallbackHelpers.isObjectMenuConfig(currentObject)){
         return null;
       };
 
-      currentObject = currentObject?.[MenuConfigKeys.menuItems][index];
+      currentObject = currentObject?.menuItems?.[index];
     };
 
     return currentObject;
   };
 
   /** handle inline submenu's by hoisting submenu action to the parent menu */
-  static flattenMenuConfig(menuConfig){
+  static flattenMenuConfig(menuConfig: MenuConfig){
     if(menuConfig == null) return null;
+
     let indexPath = [0];
     let currentParent = menuConfig;
 
@@ -56,46 +72,45 @@ class ActionSheetFallbackHelpers {
       indexPath[indexPathCount - 1]++;
     };
 
-    const getCurrentIndexPath = () => {
-      const indexPathCount = indexPath.length;
-      return indexPath[indexPathCount - 1];
-    };
+    const getCurrentIndexPath = () => 
+      (indexPath[indexPath.length - 1]);
 
-    const getCurrentParentItemsCount = () => {
-      return currentParent
-        ?.[MenuConfigKeys.menuItems]
-        ?.length ?? 0;
-    };
+    const getCurrentParentItemsCount = () => 
+      (currentParent?.menuItems?.length ?? 0);
 
-    const didReachEnd = () => {
-      return getCurrentIndexPath() >= (getCurrentParentItemsCount() - 1);
-    };
+    const didReachEnd = () => 
+      (getCurrentIndexPath() >= (getCurrentParentItemsCount() - 1));
     
     // traverse menu config until last item reached
     while(true){
       let currentItem = ActionSheetFallbackHelpers
         .getItemFromMenuConfig(menuConfig, indexPath);
 
-      if (ActionSheetFallbackHelpers.isObjectMenuConfig(currentItem)){
+      const isCurrentItemEmpty = (currentItem == null);
+
+      if (!isCurrentItemEmpty && ActionSheetFallbackHelpers.isObjectMenuConfig(currentItem!)){
         // current item is menu config, check if inline submenu
-        const isInlineSubmenu = currentItem?.[MenuConfigKeys.menuOptions]
-          ?.includes(MenuOptions.displayInline) ?? false;
+        const isInlineSubmenu = 
+          currentItem!.menuOptions?.includes('displayInline') ?? false;
 
         const inlineSubmenuItemsCount = 
-          currentItem?.[MenuConfigKeys.menuItems]?.length ?? 0;
+          currentItem!.menuItems?.length ?? 0;
 
         if(isInlineSubmenu && inlineSubmenuItemsCount > 0){
-          const inlineMenu = currentParent?.[MenuConfigKeys.menuItems]
-            .splice(getCurrentIndexPath(), 1);
+          const inlineMenu = 
+            currentParent?.menuItems!.splice(getCurrentIndexPath(), 1);
 
           const inlineMenuItems = 
-            inlineMenu?.[0]?.[MenuConfigKeys.menuItems] ?? [];
+            // @ts-ignore
+            inlineMenu?.[0]?.menuItems ?? [];
 
-          currentParent?.[MenuConfigKeys.menuItems]
-            .splice(getCurrentIndexPath(), 0, ...inlineMenuItems)
+          currentParent?.menuItems!.splice(
+            getCurrentIndexPath(), 0, ...inlineMenuItems
+          );
 
         } else {
           // update currentParent
+          // @ts-ignore
           currentParent = ActionSheetFallbackHelpers.getItemFromMenuConfig(menuConfig, indexPath);
           // and traverse down...
           indexPath.push(0);
@@ -106,9 +121,10 @@ class ActionSheetFallbackHelpers {
         indexPath.pop()
         incrementCurrentIndexPath();
 
-        const nextIndexPath  = indexPath.slice(0, indexPathCount - 1);
         const indexPathCount = indexPath.length;
+        const nextIndexPath  = indexPath.slice(0, indexPathCount - 1);
 
+        // @ts-ignore
         currentParent = ((indexPathCount <= 0)
           ? menuConfig
           : ActionSheetFallbackHelpers.getItemFromMenuConfig(menuConfig, nextIndexPath)
@@ -117,7 +133,7 @@ class ActionSheetFallbackHelpers {
         // last menu config item reached, stop...
         if(didReachEnd() || indexPath.length < 1) break;
 
-      } else if(ActionSheetFallbackHelpers.isObjectMenuAction(currentItem)){
+      } else if(ActionSheetFallbackHelpers.isObjectMenuAction(currentItem!)){
         // current item is menu action, skip...
         incrementCurrentIndexPath();
         
@@ -131,18 +147,18 @@ class ActionSheetFallbackHelpers {
   };
   
   /** returns the index of the first destructive action */
-  static getdestructiveButtonIndex(menuItems){
+  static getdestructiveButtonIndex(menuItems: MenuConfig['menuItems']){
     if(menuItems == null) return null;
     for (let index = 0; index < menuItems.length; index++) {
       const menuItem = menuItems[index];
 
-      const isDestructiveAction = menuItem
-        ?.[MenuActionKeys.menuAttributes]
-        ?.includes(MenuElementAtrributes.destructive) ?? false;
+      // @ts-ignore
+      const isDestructiveAction = menuItem?.menuAttributes
+        ?.includes('destructive') ?? false;
 
-      const isDestructiveMenu = menuItem
-        ?.[MenuConfigKeys.menuOptions]
-        ?.includes(MenuOptions.destructive) ?? false;
+      // @ts-ignore
+      const isDestructiveMenu = menuItem?.menuOptions
+        ?.includes('destructive') ?? false;
 
       if(isDestructiveAction || isDestructiveMenu){
         return index;
@@ -153,17 +169,17 @@ class ActionSheetFallbackHelpers {
   };
 
   /** remove disabled/hidden menu actions */
-  static filterMenuItems(menuItems = []){
+  static filterMenuItems(menuItems: MenuConfig['menuItems'] = []){
     if(menuItems == null) return [];
 
     return menuItems.filter(menuItem => {
-      const isDisabledAction = menuItem
-        ?.[MenuActionKeys.menuAttributes]
-        ?.includes(MenuElementAtrributes.disabled) ?? false;
+      // @ts-ignore
+      const isDisabledAction = menuItem?.menuAttributes
+        ?.includes('disabled') ?? false;
 
-      const isHiddenAction = menuItem
-        ?.[MenuActionKeys.menuAttributes]
-        ?.includes(MenuElementAtrributes.hidden) ?? false;
+      // @ts-ignore
+      const isHiddenAction = menuItem?.menuAttributes
+        ?.includes('hidden') ?? false;
       
       return (!isDisabledAction && !isHiddenAction);
     });
@@ -172,7 +188,7 @@ class ActionSheetFallbackHelpers {
 
 export class ActionSheetFallback {
   /** based on a `MenuConfig` object, show an `ActionSheetIOS` menu  */
-  static async show(menuConfig = {}){
+  static async show(menuConfig: MenuConfig){
     let indexPath = [];
 
     let actionSheetOptions     = [];
@@ -191,16 +207,18 @@ export class ActionSheetFallback {
         ActionSheetFallbackHelpers.getItemFromMenuConfig(menuConfigCopy, indexPath);
 
       // selected item is an action, exit...
-      if(ActionSheetFallbackHelpers.isObjectMenuAction(menuItem)){
+      if(ActionSheetFallbackHelpers.isObjectMenuAction(menuItem!)){
         return menuItem;
       };
 
       // selected item is menu, get submenu and remove hidden/disable actions
-      const menuItems = ActionSheetFallbackHelpers.filterMenuItems(menuItem?.[MenuConfigKeys.menuItems] ?? []);
+      const menuItems = ActionSheetFallbackHelpers.filterMenuItems(menuItem?.menuItems ?? []);
       // add menu items to action sheet
       actionSheetOptions = menuItems.map(item => (
-        item?.[MenuActionKeys.actionTitle] ??
-        item?.[MenuConfigKeys.menuTitle  ] ?? null
+        // @ts-ignore
+        item?.actionTitle ??
+        // @ts-ignore
+        item?.menuTitle   ?? null
       ));
 
       // set destructive index and offset by 1 bc of cancel button
