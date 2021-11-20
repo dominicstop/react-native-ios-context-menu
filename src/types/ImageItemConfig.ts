e02d96e1878840f65bf761f1c28b8f785d33823b
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2021-11-20 23:36:28 +0800

    ☯️ Add `ImageItemConfig `
    Summary: Copy over `RNIWrapperView` native component impl. from `react-native-ios-navigator`.

---
+++ src/types/ImageItemConfig.ts
@@ -0,0 +1,126 @@
+import type { Point, PointPreset, DynamicColor } from "./MiscTypes";
+
+
+/** Object returned by `Image.resolveAssetSource()` */
+export type ImageResolvedAssetSource = {
+  height: number;
+  width: number;
+  scale: number;
+  uri: string;
+};
+
+export enum ImageTypes {
+  IMAGE_ASSET    = 'IMAGE_ASSET'   ,
+  IMAGE_SYSTEM   = 'IMAGE_SYSTEM'  ,
+  IMAGE_REQUIRE  = 'IMAGE_REQUIRE' ,
+  IMAGE_EMPTY    = 'IMAGE_EMPTY'   ,
+  IMAGE_RECT     = 'IMAGE_RECT'    ,
+  IMAGE_GRADIENT = 'IMAGE_GRADIENT',
+};
+
+export type ImageRectConfig = {
+  width: number;
+  height: number;
+  fillColor: string;
+  borderRadius?: number;
+};
+
+/** Maps to `UIImage.RenderingMode`*/
+export type ImageRenderingModes = 
+  'automatic' | 'alwaysOriginal' | 'alwaysTemplate';
+
+/** `UIImage`-related */
+export type UIImageConfig = {
+  tint?: string | DynamicColor;
+  renderingMode?: ImageRenderingModes;
+};
+
+export type ImageSymbolWeight = 
+  | 'unspecified'
+  | 'ultraLight'
+  | 'thin'
+  | 'light'
+  | 'regular'
+  | 'medium'
+  | 'semibold'
+  | 'bold'
+  | 'heavy'
+  | 'black';
+
+export type ImageSymbolScale = 
+  | 'default'
+  | 'unspecified'
+  | 'small'
+  | 'medium'
+  | 'large';
+
+/** Maps to `UIImage.SymbolConfiguration` */
+type ImageSystemSymbolConfiguration = {
+  pointSize?: number;
+  weight?: ImageSymbolWeight;
+  scale?: ImageSymbolScale;
+} & ({
+  /** Requires iOS 15+ */
+  hierarchicalColor?: string | DynamicColor;
+} | {
+  /** Requires iOS 15+ */
+  paletteColors?: [string | DynamicColor];
+});
+
+export type ImageSystemConfig = ImageSystemSymbolConfiguration & {
+  systemName: string;
+};
+
+export type ImageGradientConfig = Partial<Pick<ImageRectConfig, 
+  | 'width'
+  | 'height'
+  | 'borderRadius'
+>> & {
+  /* An array defining the color of each gradient stop. */
+  colors: Array<string>;
+
+  /* Defines the location of each gradient stop. */
+  locations?: Array<number>;
+
+  /* The start point of the gradient when drawn in the layer’s coordinate space. */
+  startPoint?: Point | PointPreset;
+
+  /* The end point of the gradient when drawn in the layer’s coordinate space. */
+  endPoint?: Point | PointPreset;
+  
+  /* Style of gradient drawn by the layer. Defaults to axial. */
+  type?: 'axial' | 'conic' | 'radial'
+};
+
+export type ImageItemConfig = {
+  type: 'IMAGE_ASSET';
+  /** The corresponding key of asset item in the asset catalog */
+  imageValue: string;
+  imageOptions?: UIImageConfig;
+
+} | {
+  type: 'IMAGE_SYSTEM';
+  /** The key/name of the SF Symbols system icon */
+  imageValue: ImageSystemConfig;
+  imageOptions?: UIImageConfig;
+
+} | {
+  type: 'IMAGE_REQUIRE';
+  /** Object returned by `Image.resolveAssetSource()` */
+  imageValue: ImageResolvedAssetSource;
+  imageOptions?: UIImageConfig;
+
+} | {
+  type: 'IMAGE_EMPTY';
+
+} | {
+  type: 'IMAGE_RECT';
+  imageValue: ImageRectConfig;
+
+} | {
+  type: 'IMAGE_GRADIENT';
+  imageValue: ImageGradientConfig;
+  
+};
+
+export type ImageItemConfigTypes = ImageItemConfig['type'];
--- src/types/MiscTypes.ts
+++ src/types/MiscTypes.ts
@@ -1,3 +1,19 @@
+
+export type PointPreset = 
+  | 'top' 
+  | 'bottom' 
+  | 'left' 
+  | 'right'
+  | 'bottomLeft' 
+  | 'bottomRight' 
+  | 'topLeft' 
+  | 'topRight';
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
 /** Object return by `DynamicColorIOS` */
 export type DynamicColor = {
   dynamic: {
