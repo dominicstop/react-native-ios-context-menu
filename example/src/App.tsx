ee446c3c2baa5f365054501f22a51d13337d028f
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2021-11-26 07:26:59 +0800

    ⭐️ Example - Add `ContextMenuViewTest07`

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -28,6 +28,7 @@ import { ContextMenuViewTest03 } from './examples/ContextMenuViewTest03';
 import { ContextMenuViewTest04 } from './examples/ContextMenuViewTest04';
 import { ContextMenuViewTest05 } from './examples/ContextMenuViewTest05';
 import { ContextMenuViewTest06 } from './examples/ContextMenuViewTest06';
+import { ContextMenuViewTest07 } from './examples/ContextMenuViewTest07';
 
 
 type ExampleListItem = {
@@ -60,6 +61,7 @@ const EXAMPLE_COMPONENTS = [
   ContextMenuViewTest04,
   ContextMenuViewTest05,
   ContextMenuViewTest06,
+  ContextMenuViewTest07,
 ];
 
 const EXAMPLE_ITEMS: ExampleListItem[] = EXAMPLE_COMPONENTS.map((item, index) => ({
