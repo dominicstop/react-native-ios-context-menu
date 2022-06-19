83aecaf679407a95df7b59a574e5ecc93c58a537
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-06-20 00:19:48 +0800

    🛠 Refactor: Example - Rename `ContextMenuViewExample22`
    Summary: Rename `ContextMenuViewExample22` to `ContextMenuViewExample23`

--- example/src/screens/HomeScreen.tsx
+++ example/src/screens/HomeScreen.tsx
@@ -25,7 +25,7 @@ import { ContextMenuViewExample18 } from '../examples/ContextMenuViewExample18';
 import { ContextMenuViewExample19 } from '../examples/ContextMenuViewExample19';
 import { ContextMenuViewExample20 } from '../examples/ContextMenuViewExample20';
 import { ContextMenuViewExample21 } from '../examples/ContextMenuViewExample21';
-import { ContextMenuViewExample22 } from '../examples/ContextMenuViewExample22';
+import { ContextMenuViewExample23 } from '../examples/ContextMenuViewExample23';
 
 import { ContextMenuViewTest01 } from '../examples/ContextMenuViewTest01';
 import { ContextMenuViewTest02 } from '../examples/ContextMenuViewTest02';
@@ -71,7 +71,7 @@ const EXAMPLE_COMPONENTS = [
   ContextMenuViewExample19,
   ContextMenuViewExample20,
   ContextMenuViewExample21,
-  ContextMenuViewExample22,
+  ContextMenuViewExample23,
   ContextMenuViewTest01,
   ContextMenuViewTest02,
   ContextMenuViewTest03,
