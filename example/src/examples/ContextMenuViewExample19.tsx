f75c468fddb874d0c5bed782a00d82e168a610ec
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-06-26 06:26:35 +0800

    💫 Update: Examples - `ContextMenuViewExample19`

--- example/src/examples/ContextMenuViewExample19.tsx
+++ example/src/examples/ContextMenuViewExample19.tsx
@@ -21,7 +21,11 @@ export function ContextMenuViewExample19(props: ContextMenuExampleProps) {
           actionTitle: 'Action #1',
           actionSubtitle: 'Dummy action'
         }, {
+          // Create a deferred menu item... this will act as a placeholder
+          // and will be replaced with the actual menu items later
           type: 'deferred',
+          // if we have multiple deferred items, you can use the `deferredID`
+          // to distinguish between them
           deferredID: 'deferred-01'
         }],
       }}
@@ -31,11 +35,14 @@ export function ContextMenuViewExample19(props: ContextMenuExampleProps) {
           `actionKey: ${nativeEvent.actionKey} - actionTitle: ${nativeEvent.actionTitle}`
         );
       }}
+      // this event will fire when a deferred menu item is present...
       onRequestDeferredElement={async (deferredID, provider) => {
         switch(deferredID) {
           case 'deferred-01':
+            // dummy delay, wait for 1 second...
             await Helpers.timeout(1000);
 
+            // provide the items to add to the context menu...
             provider([{
               type: 'action',
               actionKey: 'action-01',
