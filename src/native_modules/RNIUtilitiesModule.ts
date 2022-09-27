3d112239b2a9aa8406a61032aa60abd51844aeb2
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-09-27 18:16:03 +0800

    💄 Gloss: Add Comment

--- src/native_modules/RNIUtilitiesModule.ts
+++ src/native_modules/RNIUtilitiesModule.ts
@@ -10,5 +10,6 @@ const MODULE_NAME = "RNIUtilitiesModule";
 export const RNIUtilitiesModule: RNIUtilitiesModule =
   NativeModules[MODULE_NAME];
 
-
+// Calling this will initialize `RNIUtilities.sharedBridge`.
+// Note: This module must be imported for this function to be invoked.
 RNIUtilitiesModule.initialize({});
