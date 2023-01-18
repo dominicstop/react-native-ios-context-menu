1d0acdbf0425b1bd1f61e996aeb5d3e52d603f50
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-18 09:58:59 +0800

    Revert "⚙️ Chore: Update `react-native.config.js`"
    This reverts commit b7e62197d230a51ba31de0be7e3f71404b7bb8be.

--- react-native.config.js
+++ react-native.config.js
@@ -1,18 +1,8 @@
-const ios = require('@react-native-community/cli-platform-ios');
-
-
 module.exports = {
   project: {
     android: null,
     ios: {
       project: './ios/IosContextMenu.xcodeproj',
     },
-  },
-  platforms: {
-    ios: {
-      projectConfig: ios.projectConfig,
-      dependencyConfig: ios.dependencyConfig,
-    },
-    android: null,
   },
 };
