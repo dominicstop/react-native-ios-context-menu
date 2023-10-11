68a0b00326f5ba67f61477c3465aefebd56d4fe9
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-11 20:59:09 +0800

    🛠 Refactor: `RNIContextMenuButtonModule`
    Summary: Re-write `RNIContextMenuButtonModule` to use `ExpoModules`.

--- ios/RNIContextMenuButton/RNIContextMenuButtonModule.swift
+++ ios/RNIContextMenuButton/RNIContextMenuButtonModule.swift
@@ -6,69 +6,46 @@
 //
 
 import Foundation
+import ExpoModulesCore
 import ReactNativeIosUtilities
 
 
-@objc(RNIContextMenuButtonModule)
-internal class RNIContextMenuButtonModule: NSObject {
+@available(iOS 14, *)
+public class RNIContextMenuButtonModule: Module {
   
-  @objc var bridge: RCTBridge!;
-  
-  @objc static func requiresMainQueueSetup() -> Bool {
-    // run init in bg thread
-    return false;
-  };
-  
-  @available(iOS 14, *)
-  func getContextMenuButton(_ node: NSNumber) -> RNIContextMenuButton? {
-    return RNIHelpers.getView(
-      forNode: node,
-      type   : RNIContextMenuButton.self,
-      bridge : self.bridge
-    );
-  };
-  
-  // MARK: - Module Commands: Navigator
-  // ---------------------------------
-  
-  @objc func dismissMenu(
-    _ node: NSNumber,
-    // promise blocks ------------------------
-    resolve: @escaping RCTPromiseResolveBlock,
-    reject : @escaping RCTPromiseRejectBlock
-  ){
-    DispatchQueue.main.async {
-      // get `RNIContextMenuButton` instance that matches node/reactTag
-      guard #available(iOS 14, *),
-            let contextMenuButton = self.getContextMenuButton(node) else {
-              
-        reject(nil, "Unable to get the corresponding 'RNIContextMenuButton' for node: \(node)", nil);
-        return;
-      };
+  public func definition() -> ModuleDefinition {
+    Name("RNIContextMenuButton");
+    
+    Function("dismissMenu") { (reactTag: Int) in
+      guard let bridge = RNIHelpers.bridge else { return };
+    
+      let contextMenuView = RNIHelpers.getView(
+        forNode: reactTag as NSNumber,
+        type: RNIContextMenuButton.self,
+        bridge: bridge
+      );
       
-      contextMenuButton.dismissMenu();
-      resolve(nil);
+      guard let contextMenuView = contextMenuView else { return };
+      contextMenuView.dismissMenu();
     };
-  };
-  
-  @objc func provideDeferredElements(
-    _ node: NSNumber,
-    deferredID: String,
-    menuItems: NSArray,
-    // promise blocks ------------------------
-    resolve: @escaping RCTPromiseResolveBlock,
-    reject : @escaping RCTPromiseRejectBlock
-  ){
-    DispatchQueue.main.async {
-      // get `RNIContextMenuButton` instance that matches node/reactTag
-      guard #available(iOS 14, *),
-            let contextMenuButton = self.getContextMenuButton(node) else {
-              
-        reject(nil, "Unable to get the corresponding 'RNIContextMenuButton' for node: \(node)", nil);
-        return;
-      };
+    
+    Function("provideDeferredElements") {
+      (reactTag: Int, args: Dictionary<String, Any>) in
       
-      contextMenuButton.provideDeferredElements(
+      guard let bridge = RNIHelpers.bridge,
+            let deferredID = args["deferredID"] as? String,
+            let menuItems = args["menuItems"] as? Array<Any>
+      else { return };
+    
+      let contextMenuView = RNIHelpers.getView(
+        forNode: reactTag as NSNumber,
+        type: RNIContextMenuButton.self,
+        bridge: bridge
+      );
+      
+      guard let contextMenuView = contextMenuView else { return };
+      
+      contextMenuView.provideDeferredElements(
         id: deferredID,
         menuElements: menuItems.compactMap {
           guard let dictItem = $0 as? Dictionary<String, Any> else { return nil };
@@ -80,31 +57,47 @@ internal class RNIContextMenuButtonModule: NSObject {
           );
         }
       );
-      
-      resolve(nil);
     };
-  };
-  
-  @objc func notifyComponentWillUnmount(
-    _ node: NSNumber,
-    params: NSDictionary
-  ){
-    DispatchQueue.main.async {
-      // get `RNIWrapperView` instance that matches node/reactTag
-      guard #available(iOS 14, *),
-            let contextMenuButton = self.getContextMenuButton(node) else {
-        #if DEBUG
-        print(
-            "LOG - ViewManager, RNIContextMenuButton: notifyComponentWillUnmount"
-          + " - for node: \(node)"
-          + " - no corresponding view found for node"
-          + " - the view might have already been unmounted..."
-        );
-        #endif
-        return;
+    
+    Function("notifyOnComponentWillUnmount") { (reactTag: Int) in
+      guard let bridge = RNIHelpers.bridge else { return };
+    
+      let contextMenuView = RNIHelpers.getView(
+        forNode: reactTag as NSNumber,
+        type: RNIContextMenuButton.self,
+        bridge: bridge
+      );
+      
+      guard let contextMenuView = contextMenuView else { return };
+      contextMenuView.notifyOnJSComponentWillUnmount();
+    };
+
+    View(RNIContextMenuButton.self) {
+      Events("onReactTagDidSet");
+      Events("onMenuWillShow");
+      Events("onMenuWillHide");
+      Events("onMenuWillCancel");
+      Events("onMenuDidShow");
+      Events("onMenuDidHide");
+      Events("onMenuDidCancel");
+      Events("onPressMenuItem");
+      Events("onRequestDeferredElement");
+    
+      Prop("menuConfig") { (view: RNIContextMenuButton, prop: Dictionary<String, Any>) in
+        view.menuConfigRaw = prop;
       };
       
-      contextMenuButton.notifyOnJSComponentWillUnmount();
+      Prop("isMenuPrimaryAction") { (view: RNIContextMenuButton, prop: Bool) in
+        view.isMenuPrimaryAction = prop;
+      };
+      
+      Prop("enableContextMenu") { (view: RNIContextMenuButton, prop: Bool) in
+        view.enableContextMenu = prop;
+      };
+      
+      Prop("internalCleanupMode") { (view: RNIContextMenuButton, prop: String) in
+        view.internalCleanupModeRaw = prop;
+      };
     };
   };
 };
