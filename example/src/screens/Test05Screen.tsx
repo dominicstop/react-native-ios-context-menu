6a09dbbcff9154c2bc71b9a6fb6af5aeefabde5d
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-02 03:02:21 +0800

    💫 Update: Ex - `Test05Screen`

--- example/src/screens/Test05Screen.tsx
+++ example/src/screens/Test05Screen.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { View, Image, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 
 import { ContextMenuView } from 'react-native-ios-context-menu';
+import { SHARED_ENV } from '../constants/SharedEnv';
+
+import { useNavigation } from '@react-navigation/native';
 
 // Repro for issue: #77
 // https://github.com/dominicstop/react-native-ios-context-menu/issues/77
@@ -10,58 +12,106 @@ import { ContextMenuView } from 'react-native-ios-context-menu';
 // * Bug: Pressables inside of a context menu firing navigation in a native 
 //   stack cause a crash
 
-export const Test05Screen = () => {
+function MessageBubble(props: React.PropsWithChildren){
   return (
-    <View style={styles.rootContainer}>
-      <ContextMenuView 
-        style={styles.messageContainer}
-        menuConfig={{
-          menuTitle: '',
-          menuItems: [{
-            actionKey: 'action-like',
-            actionTitle: 'Like',
-            icon: {
-              type: 'IMAGE_SYSTEM',
-              imageValue: {
-                systemName: 'heart.fill',
-              },
-              imageOptions: {
-                tint: 'red',
-                renderingMode: 'alwaysOriginal',
-              },
-            }
-          }],
-        }}
-        onPressMenuItem={({nativeEvent}) => {
-          switch(nativeEvent.actionKey) {
-            case "action-like":
-              break;
-          };
-        }}
-      >
-        <Pressable>
-          <Text>
+    <ContextMenuView 
+      style={styles.messageContainer}
+      menuConfig={{
+        menuTitle: '',
+        menuItems: [{
+          actionKey: 'action-like',
+          actionTitle: 'Like',
+          icon: {
+            type: 'IMAGE_SYSTEM',
+            imageValue: {
+              systemName: 'heart.fill',
+            },
+            imageOptions: {
+              tint: 'red',
+              renderingMode: 'alwaysOriginal',
+            },
+          }
+        }],
+      }}
+      onPressMenuItem={({nativeEvent}) => {
+        switch(nativeEvent.actionKey) {
+          case "action-like":
+            break;
+        };
+      }}
+    >
+      {props.children}
+    </ContextMenuView>
+  );
+};
+
+export const Test05Screen = () => {
+  const navigation = SHARED_ENV.enableReactNavigation 
+    ? useNavigation()
+    : undefined;
+  
+  return (
+    <ScrollView 
+      style={styles.rootContainer}
+      contentContainerStyle={styles.rootContainerContent}
+    >
+      <MessageBubble>
+        <Pressable
+          onPress={() => {
+            //@ts-ignore
+            navigation?.push('Home');
+          }}
+        >
+          <Text style={[styles.messageText, styles.messageTextLink]}>
             {'I am a link'}
           </Text>
         </Pressable>
-      </ContextMenuView>
-    </View>
+      </MessageBubble>
+
+      <MessageBubble>
+        <Text style={styles.messageText}>
+          {'I am a '}
+          <Text 
+            style={[styles.messageTextLink]}
+            onPress={() => {
+              //@ts-ignore
+              navigation?.push('Home');
+            }}
+          >
+            {'link'}
+          </Text>
+          {' inside a text'}
+        </Text>
+      </MessageBubble>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   rootContainer: { 
     flex: 1,
+  },
+  rootContainerContent: {
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    paddingBottom: 20
+    paddingHorizontal: 12,
+    paddingTop: 12,
+    paddingBottom: 24,
   },
   messageContainer: {
     flex: 0,
-    backgroundColor: 'red',
+    backgroundColor: 'pink',
     borderRadius: 10,
     overflow: 'hidden',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginBottom: 12,
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  messageTextLink: {
+    textDecorationLine: 'underline',
+    textDecorationColor: 'rgba(0,0,0,0.5)',
   },
 });
