11c86a0bfbe6d9b59b3721c53d9a19498556711c
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-02 07:54:04 +0800

    🆕 Add: `UIView+Helpers`

---
+++ Temp/UIView+Helpers.swift
@@ -0,0 +1,55 @@
+//
+//  UIView+Helpers.swift
+//  ReactNativeIosContextMenu
+//
+//  Created by Dominic Go on 11/2/23.
+//
+
+import UIKit
+
+// TODO: Move to `DGSwiftUtilities`
+extension UIView {
+  
+  var recursivelyGetAllSubviews: [UIView] {
+    var views: [UIView] = [];
+    
+    for subview in self.subviews {
+      views.append(subview);
+      views += subview.recursivelyGetAllSubviews;
+    };
+    
+    return views;
+  };
+  
+  var recursivelyGetAllGestureRecognizersFromSubviews: [UIGestureRecognizer] {
+    self.recursivelyGetAllSubviews.reduce(into: []){
+      $0 += $1.gestureRecognizers ?? []
+    };
+  };
+  
+  var recursivelyGetAllGestureRecognizers: [UIGestureRecognizer] {
+    (self.gestureRecognizers ?? []) +
+      self.recursivelyGetAllGestureRecognizersFromSubviews
+  };
+  
+  func recursivelyFindParentView(where predicate: (UIView) -> Bool) -> UIView? {
+    var currentView = self;
+    
+    while true {
+      guard let superview = self.superview else { return nil };
+      currentView = superview;
+      
+      guard predicate(currentView) else { continue };
+      return currentView;
+    };
+  };
+  
+  func recursivelyFindParentView<T>(whereType type: T) -> T? {
+    let match = self.recursivelyFindParentView(where: {
+      $0 is T;
+    })
+    
+    guard let match = match else { return nil };
+    return match as? T;
+  };
+};
