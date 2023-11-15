455b7c771230cec72b4102ae5b5e28d0886e050f
baransu <tomaszcichocinski@gmail.com>
2023-11-15 18:07:58 +0100

    chore: run example on new arch

---
+++ example/react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  assets: [],
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: [],
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: [
+        'RNIContextMenuButtonView',
+        'RNIContextMenuView',
+        'RNIWrapperView',
+      ],
+    },
+  },
+};
