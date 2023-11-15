4bb7653e3b5eb781a9fed425d81ceb218f200947
baransu <tomaszcichocinski@gmail.com>
2023-11-15 20:20:25 +0100

    chore: change react-native.config.js

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  assets: [],
   project: {
     android: {
       unstable_reactLegacyComponentNames: [],
     },
     ios: {
       unstable_reactLegacyComponentNames: [
-        'RNIContextMenuButtonView',
-        'RNIContextMenuView',
         'RNIWrapperView',
+        'RNIContextMenuButton',
+        'RNIContextMenuView',
       ],
     },
   },
