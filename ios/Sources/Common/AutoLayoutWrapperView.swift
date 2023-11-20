e7c551a71e3cc70504df510a080ef418ec21fa64
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-20 14:59:17 +0800

    💫 Update: `AutoLayoutWrapperView`

--- ios/Sources/Common/AutoLayoutWrapperView.swift
+++ ios/Sources/Common/AutoLayoutWrapperView.swift
@@ -9,22 +9,35 @@ import UIKit
 import ReactNativeIosUtilities
 
 class AutoLayoutWrapperView: UIView {
+
+  public override var frame: CGRect {
+    willSet {
+      self.updateSizeOfSubviews(newSize: newValue.size);
+    }
+  };
+
+  override func addSubview(_ view: UIView) {
+    if let detachedView = view as? RNIDetachedView {
+      detachedView.updateBounds(newSize: self.bounds.size);
+    };
+    
+    super.addSubview(view);
+  }
   
   override func layoutSubviews() {
     super.layoutSubviews();
-    
-    self.subviews.forEach {
-      guard let detachedView = $0 as? RNIDetachedView else { return };
-      detachedView.updateBounds(newSize: self.bounds.size);
-    };
+    self.updateSizeOfSubviews();
   };
   
   override func updateConstraints() {
     super.updateConstraints();
-    
+    self.updateSizeOfSubviews();
+  };
+  
+  func updateSizeOfSubviews(newSize: CGSize? = nil){
     self.subviews.forEach {
       guard let detachedView = $0 as? RNIDetachedView else { return };
-      detachedView.updateBounds(newSize: self.bounds.size);
+      detachedView.updateBounds(newSize: newSize ?? self.bounds.size);
     };
-  }
+  };
 };
