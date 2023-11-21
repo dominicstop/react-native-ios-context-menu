a91b04ea487fd7e8e1572509e380177c4066f6d7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-21 08:42:32 +0800

    ⚙️ Chore: Update `make-readme.sh`

--- make-readme.sh
+++ make-readme.sh
@@ -2,4 +2,4 @@
 
 rm -f ./README.md
 cat ./docs/README-01-Pre.md ./docs/README-02-Docs.md ./docs/README-03-Examples.md ./docs/README-04-Post.md > ./README.md
-sed -i 's/](..\/assets/\]\(.\/assets/' ./README.md
+sed -i '' 's/](..\/assets/\]\(.\/assets/' ./README.md
