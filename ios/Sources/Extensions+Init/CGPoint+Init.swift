856b30488e1972bfe40a2fc2b5544962acd26835
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-22 21:13:50 +0800

    ⭐️ Impl: Add `CGPoint+Init`

---
+++ ios/Sources/Extensions+Init/CGPoint+Init.swift
@@ -0,0 +1,23 @@
+//
+//  CGPoint+Init.swift
+//  ReactNativeIosContextMenu
+//
+//  Created by Dominic Go on 11/22/23.
+//
+
+import Foundation
+
+
+extension CGPoint {
+  
+  public init?(dict: Dictionary<String, Any>){
+    guard let x = dict["x"] as? NSNumber,
+          let y = dict["y"] as? NSNumber
+    else { return };
+    
+    self.init(
+      x: x.doubleValue,
+      y: y.doubleValue
+    );
+  };
+};
