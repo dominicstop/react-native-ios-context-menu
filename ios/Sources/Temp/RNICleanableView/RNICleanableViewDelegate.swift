6a672a0446fbf8fe80ce8b53501cd4c9377e698f
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-10 19:00:54 +0800

    ⭐️ Impl: `RNICleanableViewDelegate`

---
+++ ios/Sources/Temp/RNICleanableView/RNICleanableViewDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  RNICleanableViewDelegate.swift
+//  ReactNativeIosContextMenu
+//
+//  Created by Dominic Go on 2/10/24.
+//
+
+import Foundation
+
+public protocol RNICleanableViewDelegate: AnyObject {
+  
+  func notifyOnViewCleanupRequest(
+    sender: RNICleanableViewSenderType,
+    item: RNICleanableViewItem
+  ) -> Bool;
+  
+  func notifyOnViewCleanupCompletion();
+};
