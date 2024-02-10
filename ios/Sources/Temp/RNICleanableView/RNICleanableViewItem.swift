6a38c160b3bc1d62d9fb5f84cfb819bd7d4b7ab4
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-10 19:28:10 +0800

    💫 Update: `RNICleanableViewItem`

--- ios/Sources/Temp/RNICleanableView/RNICleanableViewItem.swift
+++ ios/Sources/Temp/RNICleanableView/RNICleanableViewItem.swift
@@ -9,11 +9,23 @@ import UIKit
 import DGSwiftUtilities
 
 
-public struct RNICleanableViewItem {
+public class RNICleanableViewItem {
   public var key: Int;
 
   public weak var delegate: RNICleanableViewDelegate?;
   public var viewsToCleanup: Array<WeakRef<UIView>>;
   
   public var shouldProceedCleanupWhenDelegateIsNil: Bool;
+  
+  public init(
+    key: Int, delegate: RNICleanableViewDelegate? = nil,
+    viewsToCleanup: Array<WeakRef<UIView>>,
+    shouldProceedCleanupWhenDelegateIsNil: Bool
+  ) {
+  
+    self.key = key;
+    self.delegate = delegate;
+    self.viewsToCleanup = viewsToCleanup;
+    self.shouldProceedCleanupWhenDelegateIsNil = shouldProceedCleanupWhenDelegateIsNil;
+  }
 };
