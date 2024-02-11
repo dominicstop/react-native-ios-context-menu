956ba9ed323b08554b602699e1e3a8a4a9e21617
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-11 19:38:03 +0800

    💫 Update: `RNIViewCleanupMode`

--- ios/Sources/Temp/RNICleanableView/RNIViewCleanupMode.swift
+++ ios/Sources/Temp/RNICleanableView/RNIViewCleanupMode.swift
@@ -6,13 +6,46 @@
 //
 
 import Foundation
+import DGSwiftUtilities
 
-public enum RNIViewCleanupMode {
+public enum RNIViewCleanupMode: EnumCaseStringRepresentable {
+  // MARK: - Static Alias
+  // --------------------
+
   public static let `default`: Self = .enabled(triggers: [
     .reactComponentWillUnmount,
     .instanceDeinit
   ]);
   
+  // MARK: - Enum Members
+  // --------------------
+  
   case disabled;
   case enabled(triggers: [RNIViewCleanupTrigger]);
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  public var triggers: [RNIViewCleanupTrigger] {
+    switch self {
+      case let .enabled(triggers):
+        return triggers;
+        
+      default:
+        return [];
+    };
+  };
+  
+  // MARK: - EnumCaseStringRepresentable
+  // -----------------------------------
+  
+  public var caseString: String {
+    switch self {
+      case .disabled:
+        return "disabled";
+        
+      case .enabled:
+        return "enabled";
+    };
+  };
 };
