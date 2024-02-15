615e9df15ead6bab4621618f203be95e228dfee9
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-16 02:24:53 +0800

    📦 Package: Ex - Add `react-native-reanimated`

--- example/babel.config.js
+++ example/babel.config.js
@@ -14,6 +14,8 @@ module.exports = function (api) {
           },
         },
       ],
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
   };
 };
