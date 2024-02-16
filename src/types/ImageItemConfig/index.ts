766be7e4a6e0b87229fcdaebee4e5c0515f32cfb
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-17 05:49:10 +0800

    🛠 Refactor: Remove Duplicate Types (`ImageItemConfig`)

--- src/types/ImageItemConfig/index.ts
+++
@@ -1,9 +0,0 @@
-
-
-export * from './ImageGradientConfig';
-export * from './ImageItemConfig';
-export * from './ImageLoadingConfig';
-export * from './ImageMiscTypes';
-export * from './ImageOptions';
-export * from './ImageRectConfig';
-export * from './ImageSystemConfig';
