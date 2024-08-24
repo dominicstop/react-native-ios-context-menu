70515c2123cec49927c561d462e2c56cc4b859e9
Dominic Go <dominic@dominicgo.dev>
2024-08-24 23:27:55 +0800

    💫 Update: Library Exports

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,28 @@ export * from './native_components/IosContextMenuViewNativeComponent';
 
 // temp
 export * from './native_components/RNIContextMenuView';
+
+export * from './components/ContextMenuView';
+// export * from './components/ContextMenuButton';
+
+export * from './context/ContextMenuViewContext';
+export * from './context/ContextMenuButtonContext';
+
+export * from './hooks/useMenuContext';
+export * from './hooks/useMenuButtonContext';
+
+export * from './constants/Enums';
+export * from './constants/LibEnv';
+
+export * from './types/AuxiliaryPreviewConfig';
+export * from './types/AuxiliaryPreviewConfigDeprecated';
+
+export * from './types/SharedMenuEvents';
+export * from './types/SharedMenuTypes';
+export * from './types/MenuConfig';
+export * from './types/MenuPreviewConfig';
+export * from './types/MenuIconConfig';
+
+// re-export image-config related types from 
+// `react-native-ios-utilities` for convenience...
+export type { ImageGradientConfig, ImageItemConfig, ImageLoadingConfig, ImageOptions, ImageRectConfig, ImageSystemConfig } from 'react-native-ios-utilities';
