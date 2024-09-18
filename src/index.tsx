22388c09d5519683e1447afcbbad554e3f18ff36
Dominic Go <dominic@dominicgo.dev>
2024-09-18 12:59:16 +0800

    ❌ Remove: Generated Template Sources

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-export { default as IosContextMenuView } from './native_components/IosContextMenuViewNativeComponent';
-export * from './native_components/IosContextMenuViewNativeComponent';
-
 // temp
 export * from './native_components/RNIContextMenuView';
 
