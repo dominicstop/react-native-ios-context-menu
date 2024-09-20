f66597c11da6129e937a030fc68f8fcb69a05567
Dominic Go <dominic@dominicgo.dev>
2024-09-20 11:58:05 +0800

    💫 Update: Ex - Add Test Screens as Routes

--- example/src/constants/RouteItems.ts
+++ example/src/constants/RouteItems.ts
@@ -1,6 +1,14 @@
 import { HomeScreen } from "../components/HomeScreen";
 import { ContextMenuViewTest01Screen } from "../examples/ContextMenuViewTest01Screen";
 
+import { Test01Screen } from "../screens/Test01Screen";
+import { Test02Screen } from "../screens/Test02Screen";
+import { Test03Screen } from "../screens/Test03Screen";
+import { Test04Screen } from "../screens/Test04Screen";
+import { Test05Screen } from "../screens/Test05Screen";
+import { Test06Screen } from "../screens/Test06Screen";
+import { Test07Screen } from "../screens/Test07Screen";
+
 import type { RouteKey } from "./RouteKeys";
 
 
@@ -13,7 +21,36 @@ export const ROUTE_ITEMS: Array<RouteEntry> = [
   {
     routeKey: 'home',
     component: HomeScreen,
-  }, {
+  },
+  {
+    routeKey: 'test01Screen',
+    component: Test01Screen,
+  },
+  {
+    routeKey: 'test02Screen',
+    component: Test02Screen,
+  },
+  {
+    routeKey: 'test03Screen',
+    component: Test03Screen,
+  },
+  {
+    routeKey: 'test04Screen',
+    component: Test04Screen,
+  },
+  {
+    routeKey: 'test05Screen',
+    component: Test05Screen,
+  },
+  {
+    routeKey: 'test06Screen',
+    component: Test06Screen,
+  },
+  {
+    routeKey: 'test07Screen',
+    component: Test07Screen,
+  },
+  {
     routeKey: 'contextMenuTest01',
     component: ContextMenuViewTest01Screen,
   },
--- example/src/constants/RouteKeys.ts
+++ example/src/constants/RouteKeys.ts
@@ -2,6 +2,13 @@
 export const ROUTE_KEYS = {
   home: 'home', 
   contextMenuTest01: 'contextMenuTest01',
+  test01Screen: 'test01Screen',
+  test02Screen: 'test02Screen',
+  test03Screen: 'test03Screen',
+  test04Screen: 'test04Screen',
+  test05Screen: 'test05Screen',
+  test06Screen: 'test06Screen',
+  test07Screen: 'test07Screen',
 };
 
 export type RouteKey = keyof (typeof ROUTE_KEYS);
