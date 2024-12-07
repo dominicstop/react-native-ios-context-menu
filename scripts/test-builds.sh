b22fc10ce913b11dd9fbf4b3a15ceb7c62e005a4
Dominic Go <dominic@dominicgo.dev>
2024-12-07 10:46:01 +0800

    ⚙️ Chore: Add Script `test-builds`

---
+++ scripts/test-builds.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+NEW_ARCH_STATIC=PENDING 
+OLD_ARCH_STATIC=PENDING 
+NEW_ARCH_DYNAMIC=PENDING 
+OLD_ARCH_DYNAMIC=PENDING
+
+log_build_status() {
+  echo "\n\n\nBUILD RESULTS...";
+  echo "Build - NEW_ARCH_STATIC: ${NEW_ARCH_STATIC}"
+  echo "Build - OLD_ARCH_STATIC: ${OLD_ARCH_STATIC}"
+  echo "Build - NEW_ARCH_DYNAMIC: ${NEW_ARCH_DYNAMIC}"
+  echo "Build - OLD_ARCH_DYNAMIC: ${OLD_ARCH_DYNAMIC}"
+  echo "\n\n"
+}
+
+clear_cache(){
+  echo '\nclearing pods + derived data...'
+  yarn run nuke:example-pods
+  yarn run nuke:derived-data 
+}
+
+log_build_status;
+clear_cache;
+
+echo '\n\nBuild - new-arch (fabric) + static: Begin...\n'
+yarn run pod-install:new-static
+yarn run build:ios ;
+
+if [ $? -eq 0 ]; then
+  NEW_ARCH_STATIC=SUCCESS;
+else
+  NEW_ARCH_STATIC=FAILED;
+fi;
+
+log_build_status
+clear_cache
+
+echo '\n\nBuild - old-arch (paper) + static: Begin...'
+yarn run pod-install:old-static
+yarn run build:ios ; 
+
+if [ $? -eq 0 ]; then
+  OLD_ARCH_STATIC=SUCCESS;
+else
+  OLD_ARCH_STATIC=FAILED;
+fi;
+
+log_build_status
+clear_cache
+
+echo '\nBuild - new-arch (fabric) + dynamic: Begin...\n'
+yarn run pod-install:new-dynamic
+yarn run build:ios ; 
+
+if [ $? -eq 0 ]; then
+  NEW_ARCH_DYNAMIC=SUCCESS;
+else
+  NEW_ARCH_DYNAMIC=FAILED;
+fi;
+
+log_build_status;
+clear_cache;
+
+echo 'Build - old-arch (paper) + static: Begin...'
+yarn run pod-install:old-dynamic
+yarn run build:ios ;
+
+if [ $? -eq 0 ]; then
+  OLD_ARCH_DYNAMIC=SUCCESS;
+else
+  OLD_ARCH_DYNAMIC=FAILED;
+fi;
+
+echo "\n\n All Builds Completed..."
+log_build_status;
