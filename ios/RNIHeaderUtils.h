025b2585ef3b476c366cd1a7aacbc739c34e9e06
Dominic Go <dominic@dominicgo.dev>
2024-12-16 08:37:28 +0800

    💫 Update: `RNI_INCLUDE_HEADER` Macro

--- ios/RNIHeaderUtils.h
+++ ios/RNIHeaderUtils.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if __has_include(<react_native_ios_utilities/header>)
+#if __has_include(<react_native_ios_utilities/react-native-ios-utilities.h>)
     #define RNI_INCLUDE_HEADER(header) <react_native_ios_utilities/header>
 #else
     #define RNI_INCLUDE_HEADER(header) <react-native-ios-utilities/header>
