1e85fb159e4fec7b9c38c75c2884c26aecde2852
Dominic Go <dominic@dominicgo.dev>
2024-12-16 10:01:40 +0800

    🐞 Fix: `RNI_INCLUDE_HEADER`

--- ios/Private/RNIHeaderUtils.h
+++ ios/Private/RNIHeaderUtils.h
@@ -4,11 +4,11 @@
 /// Link: https://github.com/dominicstop/react-native-ios-context-menu/pull/121
 ///
 #if __has_include(<react_native_ios_utilities/react-native-ios-utilities.h>)
-  #define RNI_UTILITIES_HEADER(header) <react_native_ios_utilities/header>
+  #define RNI_INCLUDE_HEADER(header) <react_native_ios_utilities/header>
     
 #elif __has_include(<react-native-ios-utilities/react-native-ios-utilities.h>)
-  #define RNI_UTILITIES_HEADER(header) <react-native-ios-utilities/header>
+  #define RNI_INCLUDE_HEADER(header) <react-native-ios-utilities/header>
   
 #else
-  #define RNI_UTILITIES_HEADER(header) header
+  #define RNI_INCLUDE_HEADER(header) header
 #endif
