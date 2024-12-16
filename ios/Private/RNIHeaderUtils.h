1c53d41d20d4ef8cf11bf73d92d0bd482e9ecfee
Dominic Go <dominic@dominicgo.dev>
2024-12-16 08:57:46 +0800

    🛠 Refactor: Move to `Private/RNIHeaderUtils`

--- ios/Private/RNIHeaderUtils.h
+++ ios/Private/RNIHeaderUtils.h
@@ -1,5 +1,8 @@
 #pragma once
 
+/// Based on `PR-#121` by: `coolsoftwaretyler`
+/// Link: https://github.com/dominicstop/react-native-ios-context-menu/pull/121
+///
 #if __has_include(<react_native_ios_utilities/react-native-ios-utilities.h>)
     #define RNI_INCLUDE_HEADER(header) <react_native_ios_utilities/header>
 #else
